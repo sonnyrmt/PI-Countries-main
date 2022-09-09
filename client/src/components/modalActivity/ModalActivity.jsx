5c7af429b327e2ba890498cd51c7e21661c2ee57
sonnyrmt <santi2357@hotmail.com>
2022-09-09 02:01:13 -0300

    form validations

--- client/src/components/modalActivity/ModalActivity.jsx
+++ client/src/components/modalActivity/ModalActivity.jsx
@@ -3,12 +3,27 @@ import { useDispatch, useSelector } from "react-redux";
 import { setModal, createActivity } from "../../redux/actions/actions";
 import { useState } from "react";
 import Error from "./errors/Error"
+import { useEffect } from "react";
 
 export const validateInputs = (input) => {
   const { name, difficulty, duration } = input;
   let errors = {};
+  if(name.length > 15 && name !== "") errors.name = 'Max length exceeded';
+  if(name.length < 2 && name !== "") errors.name = 'Min length almost to be two';
 
+  if(difficulty !== "" && isNaN(parseInt(difficulty))) errors.difficulty = 'Difficulty need to be number'
+  if(parseInt(difficulty) < 1) errors.difficulty = 'Min difficulty is one';
+  if(parseInt(difficulty) > 5) errors.difficulty = 'Max difficulty is five';
 
+  if(duration.length > 5) errors.duration = 'Max length exceeded'; 
+  if(duration.length && duration[2] !== ':') errors.duration = 'The format is hh:mm'
+  if(duration.length && !duration[3]) errors.duration = 'The format is hh:mm';
+  if(duration.length && !duration[4]) errors.duration = 'The format is hh:mm';
+  if(duration[0] && isNaN(parseInt(duration[0]))) errors.duration = `"${duration[0]}" need to be a number`;
+  if(duration[1] && isNaN(parseInt(duration[1]))) errors.duration = `"${duration[1]}" need to be a number`;
+  if(duration[3] && isNaN(parseInt(duration[3]))) errors.duration = `"${duration[3]}" need to be a number`;
+  if(duration[4] && isNaN(parseInt(duration[4]))) errors.duration = `"${duration[4]}" need to be a number`;
+  
   return errors;
 }
 
@@ -28,12 +43,28 @@ const ModalActivity = () => {
   });
   const [input, setInput] = useState({
     name: '',
-    difficulty: 1,
-    duration: 1,
+    difficulty: '',
+    duration: '',
     season: ''
   })
   const [errors, setErrors] = useState({});
+  const [isDisabled, setIsDisabled] = useState(true)
+  const [limitCountry,setLimitCountry] = useState({});
   const { filtered } = useSelector((state) => state);
+
+  useEffect(() => {
+
+    if(errors.name || errors.difficulty || errors.duration) {
+      setIsDisabled(true);
+    } else { 
+      if(!countries.names.length || !input.name.length || !input.difficulty.length || !input.duration.length || !input.season.length) {
+        setIsDisabled(true);
+      } else {
+        setIsDisabled(false)
+      }
+    }
+
+  }, [limitCountry, errors]);
   
 
   const handleClose = () => {
@@ -46,7 +77,7 @@ const ModalActivity = () => {
       names: [...countries.names, e.target[e.target.selectedIndex].text],
       countries: [...countries.countries, e.target.value]
     })
-    setErrors(max);
+    setLimitCountry(max);
 
     if(!max.countries) {
       setCountries({
@@ -64,6 +95,11 @@ const ModalActivity = () => {
   }
 
   const handleInputChange = (e) => {
+      setErrors(validateInputs({
+        ...input,
+        [e.target.name]: e.target.value
+      }))
+
       setInput({
         ...input,
         [e.target.name]: e.target.value
@@ -72,6 +108,7 @@ const ModalActivity = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log('asd')
     dispatch(createActivity(countries.countries ,input))
   }
 
@@ -93,22 +130,22 @@ const ModalActivity = () => {
                 )
               })}
             </select>
-            {errors.countries &&  <Error timedError={errors.countries} />}
+            {limitCountry.countries &&  <Error timedError={limitCountry.countries} />}
           </div>
           <div className={style.countryInput}>
             <label htmlFor="">Name :</label>
             <input className={style.input_style}  value={input.name}  onChange={handleInputChange} name="name" type="text" placeholder="Football, Basket" />
-            <Error error={'error'}/>
+            {errors.name ? <Error error={errors.name}/> : null}
           </div>
           <div className={style.countryInput}>
             <label htmlFor="">Difficulty :</label>
-            <input className={style.input_style} value={parseInt(input.difficulty)} onChange={handleInputChange} name="difficulty" type="number" min={1} max={5}  placeholder="Set difficulty" />
-            <Error error={'error'}/>
+            <input className={style.input_style} value={input.difficulty} onChange={handleInputChange} name="difficulty" type="text" min={1} max={5}  placeholder="Set difficulty" />
+            {errors.difficulty ? <Error error={errors.difficulty}/> : null}
           </div>
           <div className={style.countryInput}>
             <label htmlFor="">Duration :</label>
-            <input className={style.input_style}  value={parseInt(input.duration)}  onChange={handleInputChange} name="duration" type="hour" />
-            <Error error={'error'}/>
+            <input className={`${style.input_style} ${style.time}`}  value={input.duration} placeholder='Set duration'  onChange={handleInputChange} name="duration" type="text" />
+            {errors.duration ? <Error error={errors.duration}/> : null}
           </div>
           <div onChange={handleInputChange}  className={style.countryInput}>
             <label htmlFor="">Season :</label>
@@ -133,10 +170,10 @@ const ModalActivity = () => {
           </div>
           <div className={style.addedCountries} >
             {countries.names.length ? countries.names.map((name, index) => (
-                <span onClick={handleDelete} id={index}  key={name} >{name} {index}</span>
+                <span onClick={handleDelete} id={index}  key={name} >{name}</span>
             )): null}
           </div>
-          <input className={style.submit} type="submit"/>
+          <button className={style.submit} disabled={isDisabled} type="submit">{isDisabled ? 'Rellene todos los campos' : 'Submit'}</button>
         </div>
       </form>
     </div>
